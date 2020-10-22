a72b16e2f3c21b659108538486b651375c8f9cde
Nomes27 <nomi_smith18@hotmail.com>
2020-10-22 14:01:57 +0100

    CommentCard component created - voting on each comment functional

---
+++ nc-news-react-app/src/Components/CommentCard.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import axios from "axios";
+import ThumbUpIcon from "@material-ui/icons/ThumbUp";
+import ThumbDownIcon from "@material-ui/icons/ThumbDown";
+
+class CommentCard extends React.Component {
+  state = {
+    voteCount: 0,
+    commentid: null,
+  };
+
+  alterVote = (voteValue, id) => {
+    this.setState((prevState) => ({
+      voteCount: prevState.voteCount + voteValue,
+      commentid: id,
+    }));
+    axios.patch(`https://frontend-nc-news.herokuapp.com/api/comments/${id}`, {
+      inc_votes: voteValue,
+    });
+
+    console.log("function working");
+  };
+
+  render() {
+    const { comment } = this.props;
+
+    return (
+      <section className="comments" key={comment.comment_id}>
+        <h4>{comment.author}</h4>
+
+        <p className="comments_body">{comment.body}</p>
+        <h4>
+          <button onClick={() => this.alterVote(1, comment.comment_id)}>
+            <ThumbUpIcon fontSize="small" style={{ fill: "green" }} />
+          </button>
+
+          {comment.comment_id === this.state.commentid ? ( //if the comment_id equals the same one in state then increase the count, else just return the count
+            <>{comment.votes + this.state.voteCount}</>
+          ) : (
+            <>{comment.votes}</>
+          )}
+          <button
+            onClick={() => this.alterVote(-1, this.props.comment.comment_id)}
+          >
+            <ThumbDownIcon fontSize="small" color="secondary" />
+          </button>
+        </h4>
+      </section>
+    );
+  }
+}
+
+export default CommentCard;
--- nc-news-react-app/src/Components/Comments.jsx
+++ nc-news-react-app/src/Components/Comments.jsx
@@ -1,16 +1,13 @@
 import React from "react";
 import axios from "axios";
 import AddComment from "./AddComment";
-import ThumbUpIcon from "@material-ui/icons/ThumbUp";
-import ThumbDownIcon from "@material-ui/icons/ThumbDown";
+import CommentCard from "./CommentCard";
 
 /*import { ThumbUpIcon, ThumbDownIcon } from "@material-ui/icons";*/
 
 class Comments extends React.Component {
   state = {
     comments: [],
-    voteCount: 0,
-    commentid: null,
   };
 
   componentDidMount() {
@@ -24,45 +21,14 @@ class Comments extends React.Component {
         this.setState({ comments: data.comments });
       });
   }
-  alterVote = (voteValue, id) => {
-    this.setState((prevState) => ({
-      voteCount: prevState.voteCount + voteValue,
-      commentid: id,
-    }));
-    axios.patch(`https://frontend-nc-news.herokuapp.com/api/comments/${id}`, {
-      inc_votes: voteValue,
-    });
-
-    console.log("function working");
-  };
 
   render() {
     return (
       <>
         <h2 className="comments_title">Comments</h2>
         <AddComment id={this.props.id} />
-
-        {this.state.comments.map((comment, index) => {
-          return (
-            <section className="comments" key={comment.comment_id}>
-              <h4>{comment.author}</h4>
-              <p className="comments_body">{comment.body}</p>
-              <h4>
-                <button onClick={() => this.alterVote(1, comment.comment_id)}>
-                  <ThumbUpIcon fontSize="small" style={{ fill: "green" }} />
-                </button>
-
-                {comment.comment_id === this.state.commentid ? ( //if the comment_id equals the same one in state then increase the count, else just return the count
-                  <>{comment.votes + this.state.voteCount}</>
-                ) : (
-                  <>{comment.votes}</>
-                )}
-                <button onClick={() => this.alterVote(-1, comment.comment_id)}>
-                  <ThumbDownIcon fontSize="small" color="secondary" />
-                </button>
-              </h4>
-            </section>
-          );
+        {this.state.comments.map((comment) => {
+          return <CommentCard comment={comment} />;
         })}
       </>
     );
